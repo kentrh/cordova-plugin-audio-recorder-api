af77d592a607ec96390dafcd2ae7c75c54b24983
Kent Robin <kr.haugen@creuna.no>
2015-09-16 13:52:04 +0200

    Changed Android store location

--- src/android/com/emj365/plugins/AudioRecorderAPI.java
+++ src/android/com/emj365/plugins/AudioRecorderAPI.java
@@ -34,8 +34,7 @@ public class AudioRecorderAPI extends CordovaPlugin {
       seconds = 7;
     }
     if (action.equals("record")) {
-      outputFile = context.getDir(Environment.DIRECTORY_MUSIC, Context.MODE_WORLD_READABLE)
-        .getAbsoluteFile() + "/" + UUID.randomUUID().toString();
+      outputFile = context.getFilesDir() + "/" + UUID.randomUUID().toString() + ".m4a";
       myRecorder = new MediaRecorder();
       myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
       myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
